dc3b258aa099ffd02c69f12f82140ba88e4634c7
Yuhui Huang <mrhuangyuhui@users.noreply.github.com>
2017-10-23 16:30:59 +0800

    Update install-redis.sh

--- install-redis.sh
+++ install-redis.sh
@@ -1,7 +1,17 @@
 #!/bin/bash
 
+# 安装编译工具
+yum install -y gcc make
+
 cd /tmp
+
+# 下载安装包
 wget http://download.redis.io/releases/redis-4.0.2.tar.gz
+
+# 解压
 tar xzf redis-4.0.2.tar.gz
+
 cd redis-4.0.2
+
+# 编译安装
 make
