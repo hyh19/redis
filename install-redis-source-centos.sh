2fae2307d5d7787ca1b63965339efe4409ed3867
Yuhui Huang <mrhuangyuhui@users.noreply.github.com>
2017-10-26 13:54:33 +0800

    Update install-redis-source-centos.sh

--- install-redis-source-centos.sh
+++ install-redis-source-centos.sh
@@ -1,9 +1,7 @@
 #!/bin/bash
 
 # 安装工具软件
-yum install -y gcc make wget
-
-cd /tmp
+yum install -y wget gcc make
 
 # 下载安装包
 wget http://download.redis.io/releases/redis-4.0.2.tar.gz
@@ -13,5 +11,11 @@ tar xzf redis-4.0.2.tar.gz
 
 cd redis-4.0.2
 
-# 编译安装
+# 编译
 make
+
+# 安装
+make install PREFIX=/usr/local/redis/redis-4.0.2
+
+# 创建符号链接
+ln -s /usr/local/redis/redis-4.0.2 /usr/local/redis/default
