aea7f03981cfdbc5e4466271dc6582546b4b613e
Yuhui Huang <mrhuangyuhui@users.noreply.github.com>
2017-10-26 15:54:58 +0800

    Update install-redis-source-centos.sh

--- install-redis-source-centos.sh
+++ install-redis-source-centos.sh
@@ -8,6 +8,8 @@ DOWNLOAD_URL="http://download.redis.io/releases/${REDIS_VERSION}.tar.gz"
 DOWNLOAD_DIRECTORY="/tmp"
 # 工作目录
 WORKING_DIRECTORY="${DOWNLOAD_DIRECTORY}/${REDIS_VERSION}"
+# 安装目录
+INSTALL_DIRECTORY="/usr/local/redis/${REDIS_VERSION}"
 
 yum install -y wget gcc make
 
@@ -21,13 +23,15 @@ cd $WORKING_DIRECTORY
 
 make
 
-make install
+make install PREFIX=$INSTALL_DIRECTORY
 
 cd $WORKING_DIRECTORY
 
 # 配置并启动
 sudo REDIS_PORT=6379 \
-REDIS_CONFIG_FILE=/etc/redis/6379.conf \
-REDIS_LOG_FILE=/var/log/redis_6379.log \
-REDIS_DATA_DIR=/var/lib/redis/6379 \
-REDIS_EXECUTABLE=`command -v redis-server` ./utils/install_server.sh
+REDIS_CONFIG_FILE="${INSTALL_DIRECTORY}/etc/6379.conf" \
+REDIS_LOG_FILE="${INSTALL_DIRECTORY}/log/6379.log" \
+REDIS_DATA_DIR="${INSTALL_DIRECTORY}/data/6379" \
+REDIS_EXECUTABLE="${INSTALL_DIRECTORY}/bin/redis-server" ./utils/install_server.sh
+
+ln -s $INSTALL_DIRECTORY /usr/local/redis/default
